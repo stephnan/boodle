5e177494384330298c616f42e395aa57d4d34ac1
Manuel Uberti <manuel.uberti@inventati.org>
2017-09-24 10:01:22 +0200

    Remove useless namespace

--- test/clj/boodle/utils/dates_test.clj
+++
@@ -1,8 +0,0 @@
-(ns boodle.utils.dates-test
-  (:require [boodle.utils.dates :as d]
-            [clojure.test :refer :all]))
-
-(deftest test-format-date
-  (testing "Testing date formatting"
-    (let [date (java)]
-      (is (= (n/en->ita x) "3,5")))))
