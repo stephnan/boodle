27248084ba6bee86d9e88edf466d4f51b8742126
Manuel Uberti <manuel.uberti@inventati.org>
2017-10-27 12:22:21 +0200

    Add amount generator

--- test/clj/boodle/utils/numbers_test.clj
+++ test/clj/boodle/utils/numbers_test.clj
@@ -9,23 +9,23 @@
   [s]
   (not (nil? (re-matches #"\d+,\d+" s))))
 
+(def gen-amount
+  (gen/double* {:infinite? false :NaN? false :min 1 :max 10000}))
+
 (defspec converted-amount-always-contains-comma
   100
-  (prop/for-all
-   [amount (->> (gen/double* {:infinite? false :NaN? false :min 1 :max 10000})
-                (gen/fmap n/en->ita))]
-   (converted-amount? amount)))
+  (prop/for-all [amount (gen/fmap n/en->ita gen-amount)]
+    (converted-amount? amount)))
 
 (def gen-expense
   (gen/hash-map
    :item gen/string-alphanumeric
-   :amount (gen/double* {:infinite? false :NaN? false :min 1 :max 10000})))
+   :amount gen-amount))
 
 (defspec expense-must-contain-converted-amount
   100
-  (prop/for-all
-   [expense gen-expense]
-   (->> expense
-        n/convert-amount
-        (:amount)
-        converted-amount?)))
+  (prop/for-all [expense gen-expense]
+    (->> expense
+         n/convert-amount
+         (:amount)
+         converted-amount?)))
