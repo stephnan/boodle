2406324b49f6cd6911e3094136a2f38e715e2c39
Manuel Uberti <manuel.uberti@inventati.org>
2017-10-27 12:07:09 +0200

    Use property-based testing to check expenses

--- test/clj/boodle/utils/numbers_test.clj
+++ test/clj/boodle/utils/numbers_test.clj
@@ -1,19 +1,31 @@
 (ns boodle.utils.numbers-test
   (:require [boodle.utils.numbers :as n]
-            [clojure.test :refer :all]
             [clojure.test.check.clojure-test :refer [defspec]]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]))
 
+(defn converted-amount?
+  "Check if `s` contains \",\"."
+  [s]
+  (not (nil? (re-matches #"\d+,\d+" s))))
+
 (defspec converted-amount-always-contains-comma
   100
   (prop/for-all
-   [v (->> (gen/double* {:infinite? false :NaN? false :min 1 :max 10000})
-           (gen/fmap n/en->ita)
-           (gen/vector))]
-   (every? (complement nil?) (map #(re-matches #"\d+,\d+" %) v))))
+   [amount (->> (gen/double* {:infinite? false :NaN? false :min 1 :max 10000})
+                (gen/fmap n/en->ita))]
+   (converted-amount? amount)))
 
-(deftest test-convert-amount
-  (testing "Testing amount conversion"
-    (let [expense {:item "test" :amount 3.50}]
-      (is (= (n/convert-amount expense) {:item "test" :amount "3,5"})))))
+(def gen-expense
+  (gen/hash-map
+   :item gen/string-alphanumeric
+   :amount (gen/double* {:infinite? false :NaN? false :min 1 :max 10000})))
+
+(defspec expense-must-contain-converted-amount
+  100
+  (prop/for-all
+   [expense gen-expense]
+   (->> expense
+        n/convert-amount
+        (:amount)
+        converted-amount?)))
