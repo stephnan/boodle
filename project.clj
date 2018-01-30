e4db13be3767859161459462f4397f21c99481d0
Manuel Uberti <manuel.uberti@inventati.org>
2018-01-30 13:30:54 +0100

    Bump re-frame-trace

--- project.clj
+++ project.clj
@@ -59,7 +59,7 @@
   {:dev
    {:dependencies [[binaryage/devtools "0.9.9"]
                    [com.cemerick/piggieback "0.2.2"]
-                   [day8.re-frame/trace "0.1.16"]
+                   [day8.re-frame/trace "0.1.17"]
                    [figwheel-sidecar "0.5.14"]]
     :plugins [[lein-figwheel "0.5.14"]]}}
 
