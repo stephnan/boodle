e41dc278d189b94fc7a0292a7d50cc37118c2b7a
Manuel Uberti <manuel.uberti@inventati.org>
2018-02-09 08:42:59 +0100

    Add .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((cider-refresh-before-fn . "mount.core/stop")
+         (cider-refresh-after-fn . "mount.core/start"))))
