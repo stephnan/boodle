fad40b50cfcb0ce6002547038400236ce4c54b49
Manuel Uberti <manuel.uberti@inventati.org>
2018-02-09 09:01:14 +0100

    Disable emacs-lisp-checkdoc for .dir-locals.el

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,8 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil . ((cider-refresh-before-fn . "mount.core/stop")
-         (cider-refresh-after-fn . "mount.core/start"))))
+((nil
+  (cider-refresh-before-fn . "mount.core/stop")
+  (cider-refresh-after-fn . "mount.core/start"))
+ (emacs-lisp-mode
+  (flycheck-disabled-checkers . "emacs-lisp-checkdoc")))
