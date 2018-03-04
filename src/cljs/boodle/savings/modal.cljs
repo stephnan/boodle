782253e46bf9eb0188a520935607b71503c9edcd
Manuel Uberti <manuel.uberti@inventati.org>
2018-03-04 22:08:21 +0100

    Return functions instead of values

--- src/cljs/boodle/savings/modal.cljs
+++ src/cljs/boodle/savings/modal.cljs
@@ -6,82 +6,88 @@
 
 (defn save-saving
   [title save-event]
-  (let [row @(rf/subscribe [:savings-row])]
-    [:div.modal-content
-     [:div.modal-header.panel-heading
-      [:h5.modal-title title]]
-     [:div.modal-body
-      [v/validation-msg-box]
-      [:div.form
-       [:div.row
-        [:div.six.columns
-         [:label (translate :it :savings/modal.item)]
-         [:input.u-full-width
-          {:type "text"
-           :value (v/or-empty-string (:item row))
-           :on-change #(rf/dispatch [:saving-change-item
-                                     (-> % .-target .-value)])}]]
-        [:div.six.columns
-         [:label (translate :it :savings/modal.amount)]
-         [:input.u-full-width
-          {:type "text"
-           :value (:amount row)
-           :on-change #(rf/dispatch [:saving-change-amount
-                                     (-> % .-target .-value)])}]]]]]
-     [:hr]
-     [:div.modal-footer
-      [:div.modal-buttons
-       [:div.row
-        [:div.eight.columns
-         [:button.button
-          {:type "button" :title (translate :it :button/cancel)
-           :on-click #(rf/dispatch [:close-modal])}
-          (translate :it :button/cancel)]]
-        [:div.three.columns
-         [:button.button.button-primary
-          {:type "button" :title (translate :it :button/ok)
-           :on-click #(rf/dispatch save-event)}
-          (translate :it :button/ok)]]]]]]))
+  (fn []
+    (let [row @(rf/subscribe [:savings-row])]
+      [:div.modal-content
+       [:div.modal-header.panel-heading
+        [:h5.modal-title title]]
+       [:div.modal-body
+        [v/validation-msg-box]
+        [:div.form
+         [:div.row
+          [:div.six.columns
+           [:label (translate :it :savings/modal.item)]
+           [:input.u-full-width
+            {:type "text"
+             :value (v/or-empty-string (:item row))
+             :on-change #(rf/dispatch [:saving-change-item
+                                       (-> % .-target .-value)])}]]
+          [:div.six.columns
+           [:label (translate :it :savings/modal.amount)]
+           [:input.u-full-width
+            {:type "text"
+             :value (:amount row)
+             :on-change #(rf/dispatch [:saving-change-amount
+                                       (-> % .-target .-value)])}]]]]]
+       [:hr]
+       [:div.modal-footer
+        [:div.modal-buttons
+         [:div.row
+          [:div.eight.columns
+           [:button.button
+            {:type "button"
+             :title (translate :it :button/cancel)
+             :on-click #(rf/dispatch [:close-modal])}
+            (translate :it :button/cancel)]]
+          [:div.three.columns
+           [:button.button.button-primary
+            {:type "button"
+             :title (translate :it :button/ok)
+             :on-click #(rf/dispatch save-event)}
+            (translate :it :button/ok)]]]]]])))
 
 (defn transfer-amount
   [title save-event]
-  (let [row @(rf/subscribe [:transfer-row])
-        active-aims (conj @(rf/subscribe [:active-aims]) {:id "" :name ""})]
-    [:div.modal-content
-     [:div.modal-header.panel-heading
-      [:h5.modal-title title]]
-     [:div.modal-body
-      [v/validation-msg-box]
-      [:div.form
-       [:div.row
-        [:div.six.columns
-         [:label (translate :it :savings/label.active-aims)]
-         [:select.u-full-width
-          {:value (v/or-empty-string (:id-aim row))
-           :on-change #(rf/dispatch [:transfer-change-active-aim
-                                     (-> % .-target .-value)])}
-          (map common/render-option active-aims)]]
-        [:div.six.columns
-         [:label (translate :it :savings/modal.amount)]
-         [:input.u-full-width
-          {:type "text"
-           :value (:amount row)
-           :on-change #(rf/dispatch [:transfer-change-amount
-                                     (-> % .-target .-value)])}]]]]]
-     [:hr]
-     [:div.modal-footer
-      [:div.modal-buttons
-       [:div.row
-        [:div.eight.columns
-         [:button.button
-          {:type "button" :title (translate :it :button/cancel)
-           :on-click #(rf/dispatch [:close-modal])}
-          (translate :it :button/cancel)]]
-        [:div.three.columns
-         [:button.button.button-primary
-          {:type "button" :title (translate :it :button/ok)
-           :on-click #(rf/dispatch save-event)}
-          (translate :it :button/ok)]]]]]]))
+  (fn []
+    (let [row @(rf/subscribe [:transfer-row])
+          active-aims (conj @(rf/subscribe [:active-aims]) {:id "" :name ""})]
+      [:div.modal-content
+       [:div.modal-header.panel-heading
+        [:h5.modal-title title]]
+       [:div.modal-body
+        [v/validation-msg-box]
+        [:div.form
+         [:div.row
+          [:div.six.columns
+           [:label (translate :it :savings/label.active-aims)]
+           [:select.u-full-width
+            {:value (v/or-empty-string (:id-aim row))
+             :on-change #(rf/dispatch [:transfer-change-active-aim
+                                       (-> % .-target .-value)])}
+            (map common/render-option active-aims)]]
+          [:div.six.columns
+           [:label (translate :it :savings/modal.amount)]
+           [:input.u-full-width
+            {:type "text"
+             :value (:amount row)
+             :on-change #(rf/dispatch [:transfer-change-amount
+                                       (-> % .-target .-value)])}]]]]]
+       [:hr]
+       [:div.modal-footer
+        [:div.modal-buttons
+         [:div.row
+          [:div.eight.columns
+           [:button.button
+            {:type "button"
+             :title (translate :it :button/cancel)
+             :on-click #(rf/dispatch [:close-modal])}
+            (translate :it :button/cancel)]]
+          [:div.three.columns
+           [:button.button.button-primary
+            {:type "button"
+             :title (translate :it :button/ok)
+             :on-click #(rf/dispatch save-event)}
+            (translate :it :button/ok)]]]]]])))
 
 (defn save-aim
   [title save-event]
@@ -113,12 +119,14 @@
        [:div.row
         [:div.eight.columns
          [:button.button
-          {:type "button" :title (translate :it :button/cancel)
+          {:type "button"
+           :title (translate :it :button/cancel)
            :on-click #(rf/dispatch [:close-modal])}
           (translate :it :button/cancel)]]
         [:div.three.columns
          [:button.button.button-primary
-          {:type "button" :title (translate :it :button/ok)
+          {:type "button"
+           :title (translate :it :button/ok)
            :on-click #(rf/dispatch save-event)}
           (translate :it :button/ok)]]]]]]))
 
@@ -140,35 +148,40 @@
        [:div.row
         [:div.six.columns
          [:button.button
-          {:type "button" :title (translate :it :button/cancel)
+          {:type "button"
+           :title (translate :it :button/cancel)
            :on-click #(rf/dispatch [:close-modal])}
           (translate :it :button/cancel)]]
         [:div.three.columns
          [:button.button.button-primary
-          {:type "button" :title (translate :it :aims/button-delete)
+          {:type "button"
+           :title (translate :it :aims/button-delete)
            :on-click #(rf/dispatch [:delete-aim])}
           (translate :it :aims/modal.button-delete)]]]]]]))
 
 (defn mark-aim-achieved
   []
-  (let [row @(rf/subscribe [:aims-row])]
-    [:div.modal-content
-     [:div.modal-header.panel-heading
-      [:h5.modal-title (translate :it :aims/modal.achieved-title)]]
-     [:div.modal-body
-      [:p {:style {:text-align "center"}}
-       (translate :it :aims/modal.achieved-confirm)]]
-     [:hr]
-     [:div.modal-footer
-      [:div.modal-buttons
-       [:div.row
-        [:div.six.columns
-         [:button.button
-          {:type "button" :title (translate :it :button/cancel)
-           :on-click #(rf/dispatch [:close-modal])}
-          (translate :it :button/cancel)]]
-        [:div.three.columns
-         [:button.button.button-primary
-          {:type "button" :title (translate :it :button/modal.achieved-confirm)
-           :on-click #(rf/dispatch [:do-mark-aim-achieved])}
-          (translate :it :aims/modal.button-achieved)]]]]]]))
+  (fn []
+    (let [row @(rf/subscribe [:aims-row])]
+      [:div.modal-content
+       [:div.modal-header.panel-heading
+        [:h5.modal-title (translate :it :aims/modal.achieved-title)]]
+       [:div.modal-body
+        [:p {:style {:text-align "center"}}
+         (translate :it :aims/modal.achieved-confirm)]]
+       [:hr]
+       [:div.modal-footer
+        [:div.modal-buttons
+         [:div.row
+          [:div.six.columns
+           [:button.button
+            {:type "button"
+             :title (translate :it :button/cancel)
+             :on-click #(rf/dispatch [:close-modal])}
+            (translate :it :button/cancel)]]
+          [:div.three.columns
+           [:button.button.button-primary
+            {:type "button"
+             :title (translate :it :button/modal.achieved-confirm)
+             :on-click #(rf/dispatch [:do-mark-aim-achieved])}
+            (translate :it :aims/modal.button-achieved)]]]]]])))
