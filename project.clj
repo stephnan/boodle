bbde9636d1c550da4dbdc5e9c2fcc45f52ed41d0
Manuel Uberti <manuel.uberti@inventati.org>
2018-04-05 10:39:40 +0200

    Bump re-frame-10x to latest release

--- project.clj
+++ project.clj
@@ -63,7 +63,7 @@
   {:dev
    {:dependencies [[binaryage/devtools "0.9.9"]
                    [com.cemerick/piggieback "0.2.2"]
-                   [day8.re-frame/re-frame-10x "0.3.0"]
+                   [day8.re-frame/re-frame-10x "0.3.1"]
                    [day8.re-frame/tracing "0.5.1"]
                    [figwheel-sidecar "0.5.15"]]
     :plugins [[lein-figwheel "0.5.15"]]}
