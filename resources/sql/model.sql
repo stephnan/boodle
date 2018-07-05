0a26ffb3eb83b3feb33fec628e3c0dac549133e6
Manuel Uberti <manuel.uberti@inventati.org>
2018-07-05 12:08:16 +0200

    Do not capitalize name column

--- resources/sql/model.sql
+++ resources/sql/model.sql
@@ -1,10 +1,10 @@
 CREATE USER ADMIN WITH password 'admin';
 
-CREATE TABLE categories (id serial PRIMARY KEY, NAME VARCHAR(30) NOT NULL, monthly_budget NUMERIC(10,2));
+CREATE TABLE categories (id serial PRIMARY KEY, name VARCHAR(30) NOT NULL, monthly_budget NUMERIC(10,2));
 
 CREATE TABLE expenses (id serial PRIMARY KEY, DATE DATE, id_category INTEGER REFERENCES categories(id), item VARCHAR(1000), amount NUMERIC(10,2), from_savings BOOLEAN);
 
-CREATE TABLE aims (id serial PRIMARY KEY, NAME VARCHAR(30) NOT NULL, target NUMERIC(10,2) NOT NULL, achieved BOOLEAN DEFAULT FALSE);
+CREATE TABLE aims (id serial PRIMARY KEY, name VARCHAR(30) NOT NULL, target NUMERIC(10,2) NOT NULL, achieved BOOLEAN DEFAULT FALSE);
 
 CREATE TABLE transactions (id serial PRIMARY KEY, id_aim INTEGER REFERENCES aims(id), item VARCHAR(1000), amount NUMERIC(10,2), DATE DATE);
 
