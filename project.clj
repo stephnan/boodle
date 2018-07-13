61235331f0745a1b5cbc492b829957bbdd2d94d9
Manuel Uberti <manuel.uberti@inventati.org>
2018-07-13 14:02:04 +0200

    Bump postgresql

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
    [org.clojure/core.incubator "0.1.4"]
    [org.clojure/java.jdbc "0.7.7"]
    [org.clojure/tools.reader "1.3.0"]
-   [org.postgresql/postgresql "42.2.2"]
+   [org.postgresql/postgresql "42.2.3"]
    [prone "1.6.0"]
    [ring "1.6.3"]
    [ring/ring-defaults "0.3.2"]
