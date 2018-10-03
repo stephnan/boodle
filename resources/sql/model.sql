e3344c07a2346e7e8754823a7d3be36e8faebeec
Manuel Uberti <manuel.uberti@inventati.org>
2018-10-03 08:04:44 +0200

    Format SQL statements in a clearer way

--- resources/sql/model.sql
+++ resources/sql/model.sql
@@ -1,14 +1,41 @@
 CREATE USER ADMIN WITH password 'admin';
 
-CREATE TABLE categories (id serial PRIMARY KEY, name VARCHAR(30) NOT NULL, monthly_budget NUMERIC(10,2));
+CREATE TABLE categories (
+  id serial PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  monthly_budget NUMERIC(10,2)
+);
 
-CREATE TABLE expenses (id serial PRIMARY KEY, DATE DATE, id_category INTEGER REFERENCES categories(id), item VARCHAR(1000), amount NUMERIC(10,2), from_savings BOOLEAN);
+CREATE TABLE expenses (
+  id serial PRIMARY KEY,
+  DATE DATE,
+  id_category INTEGER REFERENCES categories(id),
+  item VARCHAR(1000),
+  amount NUMERIC(10,2),
+  from_savings BOOLEAN
+);
 
-CREATE TABLE aims (id serial PRIMARY KEY, name VARCHAR(30) NOT NULL, target NUMERIC(10,2) NOT NULL, achieved BOOLEAN DEFAULT FALSE);
+CREATE TABLE aims (
+  id serial PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  target NUMERIC(10,2) NOT NULL,
+  achieved BOOLEAN DEFAULT FALSE
+);
 
-CREATE TABLE transactions (id serial PRIMARY KEY, id_aim INTEGER REFERENCES aims(id), item VARCHAR(1000), amount NUMERIC(10,2), DATE DATE);
+CREATE TABLE transactions (
+  id serial PRIMARY KEY,
+  id_aim INTEGER REFERENCES aims(id),
+  item VARCHAR(1000),
+  amount NUMERIC(10,2),
+  DATE DATE
+);
 
-CREATE TABLE savings (id serial PRIMARY KEY, item VARCHAR(1000), amount NUMERIC(10,2), DATE DATE);
+CREATE TABLE savings (
+  id serial PRIMARY KEY,
+  item VARCHAR(1000),
+  amount NUMERIC(10,2),
+  DATE DATE
+);
 
 GRANT ALL PRIVILEGES ON database "boodle" TO ADMIN;
 GRANT ALL PRIVILEGES ON TABLE categories TO ADMIN;
