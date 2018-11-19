c339a54a1111ca24bfd32aec96781ef78b34882e
Manuel Uberti <manuel.uberti@inventati.org>
2018-11-19 12:45:32 +0100

    Sort settings alphabetically

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,8 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (cider-ns-refresh-before-fn . "mount.core/stop")
   (cider-ns-refresh-after-fn . "mount.core/start")
+  (cider-ns-refresh-before-fn . "mount.core/stop")
   (cider-preferred-build-tool . clojure-cli))
  (emacs-lisp-mode
   (flycheck-disabled-checkers . "emacs-lisp-checkdoc")))
