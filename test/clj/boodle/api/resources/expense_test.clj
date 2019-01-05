9680a49a7f39d3a7daf4eeec5c2554dfee162c90
Manuel Uberti <manuel@7bridges.eu>
2019-01-05 14:39:38 +0100

    Update unit test

--- test/clj/boodle/api/resources/expense_test.clj
+++ test/clj/boodle/api/resources/expense_test.clj
@@ -46,7 +46,7 @@
   (with-redefs [ur/request-body->map (fn [req] req)
                 model/update! (fn [expense] expense)]
     (let [expense {:name "test" :amount "3.50"
-                   :id-category 1 :date "14/07/2018"}]
+                   :id-category "1" :date "14/07/2018"}]
       (is (= (e/update! expense)
              {:name "test" :amount 3.50
               :id-category 1 :date (ud/to-local-date "14/07/2018")})))))
