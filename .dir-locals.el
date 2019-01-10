7509ed79e80d7ec01f3c5200257eb8725792df6c
Manuel Uberti <manuel@7bridges.eu>
2019-01-10 11:18:40 +0100

    Set cider-known-endpoints

--- .dir-locals.el
+++ .dir-locals.el
@@ -5,6 +5,7 @@
   (cider-default-cljs-repl . shadow-select)
   (cider-pprint-fn . zprint)
   (cider-preferred-build-tool . clojure-cli)
+  (cider-known-endpoints . (("hathaway" "localhost" "8777")))
   (cider-ns-refresh-after-fn . "mount.core/start")
   (cider-ns-refresh-before-fn . "mount.core/stop"))
  (emacs-lisp-mode
