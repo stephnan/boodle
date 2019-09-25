a0566f88902763f9805d1b99a4a3c5e81b5f9d0c
Manuel Uberti <manuel.uberti@inventati.org>
2019-09-25 16:01:21 +0200

    Upgrade to Gulp 4

--- resources/src/gulpfile.js
+++ resources/src/gulpfile.js
@@ -9,33 +9,32 @@ var sass = require('gulp-sass');
 sass.compiler = require('node-sass');
 
 // Compile SASS
-gulp.task('sass', function () {
+gulp.task('sass', function (done) {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./css'));
+  done();
 });
 
 // Minify compiled CSS
-gulp.task('minify-css', function() {
-    return gulp.src(['../../node_modules/font-awesome/css/font-awesome.css',
-                     '../../node_modules/bulma/css/bulma.min.css',
-                     'css/*.css'
-                    ])
-        .pipe(concat('boodle.css'))
-        .pipe(cleanCSS())
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(gulp.dest('../public/css'));
+gulp.task('minify-css', function(done) {
+  return gulp.src(['../../node_modules/font-awesome/css/font-awesome.css',
+                   '../../node_modules/bulma/css/bulma.min.css',
+                   'css/*.css'
+                  ])
+    .pipe(concat('boodle.css'))
+    .pipe(cleanCSS())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('../public/css'));
+  done();
 });
 
 // Copy vendor libraries from /node_modules into /vendor
-gulp.task('copy', function() {
-    gulp.src(['../../node_modules/font-awesome/fonts/*'])
-        .pipe(gulp.dest('../public/fonts'));
+gulp.task('copy', function(done) {
+  gulp.src(['../../node_modules/font-awesome/fonts/*'])
+    .pipe(gulp.dest('../public/fonts'));
+  done();
 });
 
 // Run everything
-gulp.task('default', ['sass', 'minify-css', 'copy']);
-
-gulp.task('dev', ['sass', 'minify-css'], function() {
-    gulp.watch('css/*.css', ['minify-css']);
-});
+gulp.task('default', gulp.series(['sass', 'minify-css', 'copy']));
