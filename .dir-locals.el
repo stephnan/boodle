417d77c25063cf898d04a8ab2cdd60ed0ad5b1d1
Manuel Uberti <manuel.uberti@inventati.org>
2020-06-07 12:32:37 +0200

    Remove references to mount

--- .dir-locals.el
+++ .dir-locals.el
@@ -4,8 +4,6 @@
 ((nil
   (cider-default-cljs-repl . shadow-select)
   (cider-preferred-build-tool . clojure-cli)
-  (cider-known-endpoints . (("hathaway" "localhost" "8777")))
-  (cider-ns-refresh-after-fn . "mount.core/start")
-  (cider-ns-refresh-before-fn . "mount.core/stop"))
+  (cider-known-endpoints . (("hathaway" "localhost" "8777"))))
  (emacs-lisp-mode
   (flycheck-disabled-checkers . "emacs-lisp-checkdoc")))
