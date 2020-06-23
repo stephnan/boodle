8b76540906b64311ad92a71cee5298a15c544be3
Manuel Uberti <manuel.uberti@inventati.org>
2020-06-23 13:50:31 +0200

    Pick REPL types manually
    This is necessary to pick the correct tool when connecting to a sibling
    process.

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,8 +2,6 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 
 ((nil
-  (cider-default-cljs-repl . shadow)
-  (cider-preferred-build-tool . clojure-cli)
   (cider-known-endpoints . (("hathaway" "localhost" "8777"))))
  (emacs-lisp-mode
   (flycheck-disabled-checkers . "emacs-lisp-checkdoc")))
